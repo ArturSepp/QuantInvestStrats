07696082a448b60b0b1fe87da0ce7eec6b7757aa
ArturSepp <39810730+ArturSepp@users.noreply.github.com>
2022-12-30 19:46:40 +0100

    add setup and local_paths.yaml

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements(file):
+    with open(file) as f:
+        return f.read().splitlines()
+
+
+def read_file(file):
+    with open(file) as f:
+        return f.read()
+
+
+long_description = read_file("README.md")
+requirements = read_requirements("requirements.txt")
+
+setup(
+    name='qis',
+    version='1.0.2',
+    author='Artur Sepp',
+    author_email='artursepp@gmail.com',
+    url='https://github.com/ArturSepp/QuantInvestStrats',
+    description='Implementation of visualisation and reporting analytics for Quantitative Investment Strategies',
+    long_description_content_type="text/x-rst",  # If this causes a warning, upgrade your setuptools package
+    long_description=long_description,
+    license="MIT license",
+    packages=find_packages(exclude=["docs"]),  # Don't include test directory in binary distribution
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
