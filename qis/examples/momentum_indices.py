9d1282665d9b57fff45f7f8c5e54b4346847976e
ArturSepp <39810730+ArturSepp@users.noreply.github.com>
2024-07-19 11:20:47 +0200

    add example of report for momentum indices

--- qis/examples/momentum_indices.py
+++ qis/examples/momentum_indices.py
@@ -1,10 +1,10 @@
 """
-run multi asset report for momentum indices
+run multi asset report for momentum indices: run with open Bloomberg terminal
 """
 import qis as qis
 from bbg_fetch import fetch_field_timeseries_per_tickers
 
-
+# define momentum indices for Bloomberg fectch
 momentum_indices = {'SPTR Index': 'S&P 500',
                     'MTUM US Equity': 'MTUM Mom Beta',
                     'SP500MUP Index': 'S&P500 Mom Beta',
@@ -12,9 +12,11 @@ momentum_indices = {'SPTR Index': 'S&P 500',
                     'PMOMENUS Index': 'BBG Market-Neutral Mom'}
 prices = fetch_field_timeseries_per_tickers(tickers=list(momentum_indices.keys())).rename(momentum_indices, axis=1).dropna()
 
+# time period for performance measurement
 time_period = qis.TimePeriod('31Dec2019', '18Jul2024')
 fig = qis.generate_multi_asset_factsheet(prices=prices,
                                          benchmark='S&P 500',
                                          time_period=time_period,
                                          **qis.fetch_default_report_kwargs(time_period=time_period))
+# save report to pdf
 qis.save_figs_to_pdf(figs=[fig], file_name=f"momentum_indices_report", orientation='landscape')
